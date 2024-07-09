17f710a6ae5e27d9f8fd60b8c6b74dd470b79223
Andrei Bacin <andreib.pf@gmail.com>
2024-07-09 09:03:01 -0300

    First commit.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hydra - Login/Register",
-  description: "Hydra login/register page.",
+  title: "Hydra - Login",
+  description: "Hydra login page.",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import Login from "@/components/Login";
-import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [register, setRegister] = useState(false);
-
   return (
     <main className={styles.main}>
       <Login />
